580c0400285e99b298833951cf78bc6312669a59
Hocine-Bec <bechebil.houcin@gmail.com>
2025-10-14 21:36:16 +0100

    feat(application): Implement address DTOs
    - Implements AddressResponse, CreateAddressRequest, and UpdateAddressRequest DTOs.
    - Changes them from empty records to classes with related properties.
    - AddressResponse includes a FullAddress property.

--- Backend/Application/DTOs/Address/CreateAddressRequest.cs
+++ Backend/Application/DTOs/Address/CreateAddressRequest.cs
@@ -1,3 +1,12 @@
 namespace Application.DTOs.Address;
 
-public record CreateAddressRequest{}
+public class CreateAddressRequest
+{
+    public string Street { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public decimal? Latitude { get; set; }
+    public decimal? Longitude { get; set; }
+}
